c272a1d2304f5512fea132357f73575691caec79
Carolina RC <caro.rmz315@gmail.com>
2018-04-23 21:29:22 -0500

    added comedian friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,178 @@
-var friends = {
-    name: "Samantha Bee", 
-    photo: "app/data/sambee.gif",
-    scores: [
+var friends = [{
 
-    ]
-},
+
+        name: "Samantha Bee",
+        photo: "app/data/sambee.gif",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+
+        ]
+    },
+
+    {
+        name: "Amy Schumer",
+        photo: "amy_s.jpg",
+        scores: [
+            "1",
+            "5",
+            "2",
+            "2",
+            "1",
+            "5",
+            "4",
+            "1",
+            "2",
+            "5"
+
+        ]
+    },
+
+    {
+        name: "Jessica Williams",
+        photo: "jwilly.jpg",
+        scores: [
+            "2",
+            "3",
+            "3",
+            "3",
+            "2",
+            "2",
+            "5",
+            "2",
+            "3",
+            "2"
+
+        ]
+    },
+
+    {
+        name: "Wanda Sykes",
+        photo: "wanda.jpg",
+        scores: [
+            "3",
+            "4",
+            "4",
+            "4",
+            "3",
+            "3",
+            "1",
+            "3",
+            "4",
+            "3"
+
+        ]
+    },
+    {
+        name: "Ali Wong",
+        photo: "ali_wong.jpg",
+        scores: [
+            "2",
+            "3",
+            "3",
+            "3",
+            "2",
+            "2",
+            "5",
+            "2",
+            "3",
+            "2"
+
+        ]
+    },
+    {
+        name: "Anjelah Johnson",
+        photo: "a_johnson.jpg",
+        scores: [
+            "3",
+            "4",
+            "4",
+            "4",
+            "3",
+            "3",
+            "1",
+            "3",
+            "4",
+            "3"
+
+        ]
+    },
+
+    {
+        name: "Dulce Sloan",
+        photo: "dulce_sloan.jpg",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+
+        ]
+    },
+    {
+        name: "Kristen Schaal",
+        photo: "k_schall.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+
+        ]
+    },
+    {
+        name: "Aparna Nancherla",
+        photo: "a_nan.jpg",
+        scores: [
+            "2",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3"
+
+        ]
+    },
+
+    {
+        name: "Teresa Choyguha",
+        photo: "tersa.jpg",
+        scores: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+
+        ]
+    },
+}]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,18 @@
-// Dependencies 
-var http = require("http");
-var path = require("path");
 
+
+// Require/import the HTTP module
+var http = require("http");
+var express = require("express")
+var bodyParser = require("body-parser")
+var path = require("path");
 // module.exports = function(app) {
 //     console.log("export worked!")
 
 // };
 
 // get route to api/friends
-app.get("api/friends", function(req, res){
-return res.json(friends);
+app.get("api/friends", function (req, res) {
+    return res.json(friends);
 
 
 });
@@ -19,32 +22,38 @@ return res.json(friends);
 //     // Grab the selected parameter
 //     var chosen = req.params.character;
 //     console.log(chosen);
-  
+
 //     // Filter to show only the selected character
 //     for (var i = 0; i < characters.length; i++) {
 //       if (chosen === characters[i].routeName) {
 //         return res.json(characters[i]);
 //       }
 //     }
-  
+
 //     // Otherwise display "No character found"
 //     return res.send("No character found");
 //   });
 
-  // ---
+// ---
 
 // no character found 
-    
+
 
 //A POST routes /api/friends
 //handle incoming survey results
-//route also be used to handle compatibility logic
 
-app.post("api/friends", function(req,res) {
+app.post("api/friends", function (req, res) {
+   
+   //req.boyd hosts is equal to the JSON post sent from the user
+   // this works because of body-parser middleware
     var newFriend = req.body;
     console.log(newFriend);
 
+    // add the json the user send to the character array
     friends.push(newFriend);
+    //display JSON to users
     res.json(newFriend);
+
+    //route also be used to handle compatibility logic
 
 });
