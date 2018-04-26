643c6d612e5a84912664763adf9605acdfed3124
Carolina RC <caro.rmz315@gmail.com>
2018-04-26 13:04:33 -0500

    add correction to html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,6 +17,11 @@ module.exports = function(app) {
         //  console.log(friendsList);
     });
 
+    // 
+
+  
+    // 
+
     // API post methods -- the below code handles when a user submits a form 
     // data (ie a JSON object ) this is pushed to a JS array
     app.post("/api/friends", function (req, res) {
@@ -30,28 +35,3 @@ module.exports = function(app) {
     });
 
 }
-
-// Displays a single character, or shows "No character found"
-// app.get("/api/characters/:character", function(req, res) {
-//     // Grab the selected parameter
-//     var chosen = req.params.character;
-//     console.log(chosen);
-
-//     // Filter to show only the selected character
-//     for (var i = 0; i < characters.length; i++) {
-//       if (chosen === characters[i].routeName) {
-//         return res.json(characters[i]);
-//       }
-//     }
-
-//     // Otherwise display "No character found"
-//     return res.send("No character found");
-//   });
-
-// ---
-
-// no character found 
-
-
-//A POST routes /api/friends
-//handle incoming survey results
