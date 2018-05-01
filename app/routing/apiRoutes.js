947f622b660a326a8b9a1416cb5edb2db2ed6d36
Carolina RC <caro.rmz315@gmail.com>
2018-05-01 08:07:48 -0500

    pseudocode to id the most compatible friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -42,17 +42,23 @@ module.exports = function(app) {
 
             res.json(newFriend);
 
-
     });
-
-}
 
 // Determine the user's most compatible friend using the following as a guide:
 // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+
+    // console.log(newFriend.scores);
+    // var scoresArr = newFriend.scores;
+    // var scoresNum = scoresArr.split(",").map(function(scoresArr) {
+    //     return parseInt(scoresArr, 10)
+    // });
+
+}
+
+//compare the difference between user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
 // Example:
 // User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 // User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
 // Total Difference: 2 + 1 + 2 = 5
-// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
+
 // The closest match will be the user with the least amount of difference
